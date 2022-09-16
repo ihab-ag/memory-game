b04eca066af95d49d652a40347073fd3e724c497
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-16 15:17:18 +0300

    function to shuffle cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 window.onload=()=>{
     // declarables
     let cards=['html','css','js','css','js','html'];
-    
+    // functions
+    // suffle cards order
+    const shuffle=()=>{
+        cards = cards.sort(() => 0.5 - Math.random());
+    }
     
 }
