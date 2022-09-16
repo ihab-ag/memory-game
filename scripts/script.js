02f351dfcb3faac3dd72521addbcb79f4449b264
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-16 15:45:29 +0300

    addedd function to flip card by id

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
 window.onload=()=>{
     // declarables
     let cards=['html','css','js','css','js','html'];
+    let selected='';
+    let id ;
     // functions
     // suffle cards order
     const shuffle=()=>{
         cards = cards.sort(() => 0.5 - Math.random());
+    }
+    // select function
+    const select=(type,id)=>{
+        cardFlip(id);
+        if(selected==type){
+
+        }
+    }
+    // flip card
+    const cardFlip=(id)=>{
+        document.getElementById(id).style.transform='rotateY(180deg)';
     }
     
 }
